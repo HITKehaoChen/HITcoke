1a31e4d2fa05b3289d4350f24cad00dfd6cd0e86
littlecoke <tgyzckh@hotmail.com>
2017-04-15 10:02:31 +0800

    new one

---
+++ RepeatingDecimalsACMICPCWORLDFINALS1990UVa202.cpp
@@ -0,0 +1,40 @@
+
+//Repeating Decimals,ACM/ICPC WORLDFINALS 1990 UVa202
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+int r[3003],u[3003],s[3003];
+
+int main()
+{
+	int n,m,t;
+	while (cin >> n >> m) {
+		t = n;
+		memset(r, 0, sizeof(r));
+		memset(u, 0, sizeof(u));
+		int count = 0;
+		r[count ++] = n/m;
+		n = n%m;
+		while (!u[n] && n) {
+			u[n] = count;
+			s[count] = n;
+			r[count ++] = 10*n/m;
+			n = 10*n%m;
+		}
+		printf("%d/%d = %d",t,m,r[0]);
+		printf(".");
+		for (int i = 1 ; i < count && i <= 50 ; ++ i) {
+			if (n && s[i] == n) printf("(");
+			printf("%d",r[i]);
+		}
+		if (!n) printf("(0");
+		if (count > 50) printf("...");
+		printf(")\n");
+		printf("   %d = number of digits in repeating cycle\n\n",!n?1:count-u[n]);
+	}
+	return 0;
+}
